211ab8b8419a66fa2529631a439a5e526cab437e
tahreeminamdar22 <tahreeminamdar@gmail.com>
2025-02-09 20:04:10 +0530

    styles addded

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 const __filename= fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
